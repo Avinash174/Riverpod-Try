3eacc207d2dc6e6c0171a381a72d4195f0861f71
Avinash Magar <avinashmagar15@gmail.com>
2025-03-14 11:00:49 +0530

    new chnages done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_try/homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +27,7 @@ class _MyAppState extends State<MyApp> {
           ),
           useMaterial3: true,
         ),
-        // home: HomeScreen(),
+        home: const HomeScreen(),
       ),
     );
   }
